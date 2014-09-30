5acd26910c5721b4665337bbb1a646cfad225d87
Aaron Lobb <alobb@hmc.edu>
2014-09-30 14:51:04 -0700

    Added adapter for bulletin board

---
+++ app/src/main/java/team6/cs121/bulletinboard/NoteAdapter.java
@@ -0,0 +1,57 @@
+package team6.cs121.bulletinboard;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by alobb on 9/29/14.
+ */
+public class NoteAdapter extends ArrayAdapter<Note> {
+    private final Context context;
+    private final List<Note> values;
+
+
+    public NoteAdapter(Context context, int resource, List<Note> values) {
+        super(context, resource);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.note, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.note_textview);
+        textView.setText(this.values.get(position).getText());
+        // change the icon for Windows and iPhone
+
+        return rowView;
+    }
+
+    @Override
+    public int getCount () {
+        return values.size();
+    }
+
+    @Override
+    public long getItemId (int position) {
+        return position;
+    }
+
+    @Override
+    public Note getItem (int position) {
+        return values.get(position);
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
+    }
+}
